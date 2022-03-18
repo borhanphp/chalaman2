c4e2e603c3dd91ac23a6f643a83dd23e9ca25283
borhan uddin <borhan uddin>
2022-03-18 11:24:16 +0600

    build folder adedd

--- server.js
+++ server.js
@@ -37,9 +37,9 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 //cors
-if(process.env.NODE_ENV === 'development'){
-    app.use(cors({origin: `${process.env.CLIENT_URL}`}));
-}
+// if(process.env.NODE_ENV === 'development'){
+//     app.use(cors({origin: `${process.env.CLIENT_URL}`}));
+// }
 app.use(cors(corsOptions));
 
 //routes middleware
